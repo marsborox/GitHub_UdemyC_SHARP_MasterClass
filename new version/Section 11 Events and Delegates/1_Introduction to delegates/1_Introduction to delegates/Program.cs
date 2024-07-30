2e3f519b9a73f60a9e151fb6199b04a637a4440c
marsborox <162184787+marsborox@users.noreply.github.com>
2024-07-30 11:34:50 +0200

    added delegates

---
+++ new version/Section 11 Events and Delegates/1_Introduction to delegates/1_Introduction to delegates/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _1_Introduction_to_delegates
+{
+    //declaration of delegate can be outside of class as well
+    public delegate void Notify3(string msg);
+    internal class Program
+    {
+        //1. Declaration
+        public delegate void Notify(string message);
+        static void Main(string[] args)
+        {
+            //Delegates define a method signature,
+            //and any method assigned to a delegate must match the signature
+
+            // 2. INstantination
+            //Notify , name of delegate
+            Notify notifyDelegate = ShowMessage;
+
+            //old way dont do anymore
+            Notify notifyDelegate2= new Notify (notifyDelegate);
+
+            // 3. Invocation
+
+            notifyDelegate("Message");
+        }
+        static void ShowMessage(string message)
+        {//we use this method to isntantiatethe delegate
+            Console.WriteLine( message);
+        }
+    }
+}
