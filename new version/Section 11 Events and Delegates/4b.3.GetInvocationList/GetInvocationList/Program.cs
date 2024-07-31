a840c21522f30d3b9de4ce34a9ce7caa4dc90737
marsborox <162184787+marsborox@users.noreply.github.com>
2024-07-31 22:11:02 +0200

    GetInvocatioNList, publisher and subscriber
    how to do invokes safely
    intro to publisher and subscriber

---
+++ new version/Section 11 Events and Delegates/4b.3.GetInvocationList/GetInvocationList/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace GetInvocationList
+{
+    public delegate void LogHandler(string message);
+    //So if you want to use the log handler, we need to make sure that the method that we assign to it follows
+    //the same declaration.
+    public class Logger
+    {//here we define methods
+        public void LogToConsole(string message)
+        {
+            Console.WriteLine("Console Log: " + message);
+        }
+        public void LogToFile(string message)
+        {
+            Console.WriteLine("File log: " + message);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = new Logger();
+            //here we take LogToConsole method and assign it to variable
+            LogHandler logHandler = logger.LogToConsole;
+
+            //this wil lcreate multicast delegate - add method to it
+            logHandler += logger.LogToFile;
+
+            //this will now invoke all methods in multicast delegate
+            // unsafely
+            logHandler("Log this info!");
+
+            //this invocation list will give us the items inside of the delegate
+            foreach (LogHandler handler in logHandler.GetInvocationList())
+            {
+                try
+                {
+                    handler("Event occured with error handling");
+                }
+                catch (Exception ex) 
+                {
+                    Console.WriteLine("Exception caught: "+ex.Message);
+                }
+            }
+
+            /*
+            //we can remove as well
+            logHandler -= logger.LogToFile;
+            */
+
+            if (IsMethodInDelegate(logHandler, logger.LogToFile))
+            {//if method is in delegate then remove it from delegate
+                logHandler -= logger.LogToFile;
+                Console.WriteLine("LogToFile method removed");
+            }
+            else 
+            {//so if we would never added LogToFile method to delegate 
+                //we would get error
+                Console.WriteLine("LogToFile Method not found!");
+            }
+            if (logHandler != null)
+            {
+                InvokeSafely(logHandler, "After removing LogToFile");
+            }
+        }
+        static void InvokeSafely(LogHandler logHandler, string message)
+        {
+            LogHandler tempLogHandler = logHandler;
+            if(tempLogHandler != null) 
+            {
+                tempLogHandler(message);
+            }
+        }
+        //this pretty much checks if method is in delegate
+        static bool IsMethodInDelegate(LogHandler logHandler, LogHandler method)
+        {
+            if (logHandler == null)
+            {//do we at least have osmething in logHandler?
+                return false;
+            }
+            foreach (var d in logHandler.GetInvocationList())
+            {//is everything in logHandler delegate
+
+                if (d == (Delegate)method)//we take method and cast it into delegate
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ new version/Section 11 Events and Delegates/4b.4. Intro PubSub/4b.4. Intro PubSub/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _4b._4._Intro_PubSub
+{
+    public delegate void Notify(string message);
+    // the "Notify" is signature of methods that should handle the event
+    public class EventPublisher
+    {//The "On" prefix makes it immediately clear that the method
+        //is associated with an event.
+        //It signifies that the method is not just a regular method but
+        //one that is called when a specific event occurs.
+        public event Notify OnNotify;
+
+        public void RaiseEvent(string message)
+        {
+            OnNotify?.Invoke(message);
+            //we are going to invoke the event if there are subscribers
+            //they may be none subscribers, thats why the "?"
+        }
+    }
+    public class EventSubscriber
+    {
+        public void OnEventRaised(string message)
+        {
+            //when event is raised then we will 
+            //do this - here is what we want to happen
+            Console.WriteLine("Event received");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //then we prepare publisher and subscriber
+            EventPublisher publisher = new EventPublisher();
+            EventSubscriber subscriber = new EventSubscriber();
+
+            // we set up publisher on notify and subscribe with our event raised method
+            //that will be called when event is raised
+            //this is how we subscribe to an event
+
+            publisher.OnNotify += subscriber.OnEventRaised;
+            //and when event is raised then we will call
+            //what is inside of OnEventRaised method - its allways on EventRaised
+            //probably but the content is what we will put there
+
+            //once the raise event of publisher method is called 
+            //then we are raising the event
+            //and we are executing  whatever methods are listening to us
+
+            publisher.RaiseEvent("test");
+        }
+    }
+}
