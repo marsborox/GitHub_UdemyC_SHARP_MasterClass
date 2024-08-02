288945937e6617db18f51d0b2e9cfedeb5e2d222
marsborox <162184787+marsborox@users.noreply.github.com>
2024-08-02 22:10:24 +0200

    Polymorphism intro

---
+++ new_version/Section9 interfaces and polymorphism/2 Polymorphism/2 Polymorphism/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _2_Polymorphism
+{
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Generic animal sound");
+        }
+    }
+    public class Dog:Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//we can treat dog like animal like this
+            //what matters is that second part is it dog,cat,....
+            Animal myDog = new Dog();
+            Animal myCat = new Cat();
+
+            myDog.MakeSound();
+
+            myCat.MakeSound();
+
+            Console.WriteLine();
+        }
+    }
+}
