b98721ca56f6713bcc51deb88b9452a9659e9b6a
mierka_m <162184787+marsborox@users.noreply.github.com>
2025-04-28 13:27:56 +0200

    we were learning interfaces

---
+++ new_version/Section9 interfaces and polymorphism/3 PolymorphicInterfaces/TrueFalse/TrueFalse/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace TrueFalse
+{
+    internal class Program
+    {
+        bool True = false;
+        bool False = true;
+
+        bool TruthFinder()
+        {
+            if (!False)
+            {
+                return true;
+            }
+            if (!True)
+            {
+                return false;
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
